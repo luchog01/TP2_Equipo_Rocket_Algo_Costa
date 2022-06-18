11eb7958a4a2511c69983d06c3ad99715ff272e0
Maximiliano Otero <motero@fi.uba.ar>
2022-06-18 15:10:12 -0300

    Adding function show_playlist

--- youtube_api.py
+++ youtube_api.py
@@ -10,7 +10,7 @@ from google.oauth2.credentials import Credentials
 from requests import request
 import httplib2
 
-# UCd_QeeJYwLmb13KUP0E3FHw channel id
+CHANNEL_ID = 'UCd_QeeJYwLmb13KUP0E3FHw'
 # Primera Playlist PLIG13vm2QTYwCFv8lDl0SOiq1Tjwie_Ko
 
 SCOPES = [
@@ -18,8 +18,8 @@ SCOPES = [
 ]
 
 # Archivo generado para la API
-ARCHIVO_SECRET_CLIENT = 'credentials.json'
-ARCHIVO_TOKEN = 'token.json'
+FILE_SECRET_CLIENT = 'credentials.json'
+FILE_TOKEN = 'token.json'
 
 def clear() -> None:
     '''
@@ -27,7 +27,7 @@ def clear() -> None:
     '''
     #Windows
     if os.name == 'nt':
-        os.system('cls')   
+        os.system('cls')
     #Linux/Mac
     else:
         os.system('clear')
@@ -38,40 +38,40 @@ def login() -> Resource:
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists(ARCHIVO_TOKEN):
-        creds = Credentials.from_authorized_user_file(ARCHIVO_TOKEN, SCOPES)
+    if os.path.exists(FILE_TOKEN):
+        creds = Credentials.from_authorized_user_file(FILE_TOKEN, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                ARCHIVO_SECRET_CLIENT, SCOPES)
+                FILE_SECRET_CLIENT, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(ARCHIVO_TOKEN, 'w') as token:
+        with open(FILE_TOKEN, 'w') as token:
             token.write(creds.to_json())
 
     return build("youtube", "v3", credentials=creds)
 
 
 def new_playlist(conn: Resource) -> None:
-    
+
     """
     Create a new playlist in youtube asking for its name, description and privacy status
     """
 
     clear()
-    
-    check1 = True
+
+    check1: bool = True
     while check1:
         playlist_name :str = input('Input a name for your new playlist [Max 150 char]: ')
         if len(playlist_name) <= 150:
             check1 = False
         else:
-            print('Playlist name must be less than 150 characters')    
+            print('Playlist name must be less than 150 characters')
 
-    check2 = True
+    check2: bool = True
     while check2:
         playlist_description :str = input('Input a description for your new playlist [Max 5000 char]: ')
         if len(playlist_name) <= 5000:
@@ -79,30 +79,57 @@ def new_playlist(conn: Resource) -> None:
         else:
             print('Playlist description must be less than 5000 characters')
 
-    check3 = True
-    while check3:        
+    check3: bool = True
+    while check3:
         privacy: str = input('Playlist must be public, private or unlisted?')
         if privacy.lower() in ['public','private','unlisted']:
             check3 = False
             privacy = privacy.lower()
         else:
             print('Input a valid privacy status')
-            
+
     try:
         conn.playlists().insert(
-            part="snippet,status",
-            body=dict(
-            snippet=dict(
-                title=playlist_name,
-                description=playlist_description
+            part = "snippet,status",
+            body = dict(
+            snippet = dict(
+                title = playlist_name,
+                description = playlist_description
                 ),
-            status=dict(
-            privacyStatus=privacy
+            status = dict(
+            privacyStatus = privacy
                 )
             )
         ).execute()
-        
+
         print(f'\nNew playlist called {playlist_name} created succesfully.')
-        
+
     except:
         print('\nAn error has occurred')
+
+
+def show_playlists(conn: Resource) -> None:
+    """
+    Show playlists [Max 50] on channel id
+    """
+    
+    clear()
+    
+    request = conn.playlists().list(
+        part = "snippet",
+        channelId = CHANNEL_ID,
+        maxResults = 50
+    )
+    response = request.execute()
+    playlists_quantity: int = response['pageInfo']['totalResults']
+
+    playlists: list = []
+    response = response['items']
+
+    for j in range(len(response)):
+        playlist_title: str = response[j]['snippet']['title']
+        playlists.append(playlist_title)
+
+    print(f'The user has {playlists_quantity} titled playlists on youtube:\n')
+    for i in range(len(playlists)):
+        print(f'{i + 1}. {playlists[i]}')

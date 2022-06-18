f3610ec8d840f860588a40df449534ef0d85be39
Maximiliano Otero <motero@fi.uba.ar>
2022-06-18 16:45:28 -0300

    Adding functions new_playlist and show_playlists

--- spotify_api.py
+++ spotify_api.py
@@ -2,17 +2,29 @@ import tekore as tk
 from tekore import Spotify
 import os
 
+USER_ID = '31wehtihuhvriaasxyd5phnekdye'
 CLIENT_ID = '5422aa04b10040e18ef47834e08ec9aa'
 CLIENT_SECRET = 'a03bc8e3402949e480f4eb98036a230a'
 REDIRECT_URI = 'https://example.com/callback' 
 FILE_TEKORE = 'tekore.cfg' 
+
+def clear() -> None:
+    '''
+    Clear the console for your operating system
+    '''
+    #Windows
+    if os.name == 'nt':
+        os.system('cls')
+    #Linux/Mac
+    else:
+        os.system('clear')
 
 
 def login() -> Spotify:
     if os.path.exists(FILE_TEKORE):
         conf = tk.config_from_file(FILE_TEKORE, return_refresh=True)
         token = tk.refresh_user_token(*conf[:2], conf[3])
-        spotify = tk.Spotify(token)
+        spotify: Spotify = tk.Spotify(token)
     else:
         conf: tuple = (CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
         token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
@@ -20,4 +32,62 @@ def login() -> Spotify:
         spotify: Spotify = tk.Spotify(token)
 
     return spotify
-   
+
+
+def new_playlist(conn: Spotify) -> None:
+    """
+    Create a new playlist in spotify asking for its name, description and privacy status
+    """
+
+    clear()
+
+    check1: bool = True
+    while check1:
+        playlist_name :str = input('Input a name for your new playlist [Max 100 char]: ')
+        if len(playlist_name) <= 100:
+            check1 = False
+        else:
+            print('Playlist name must be less than 100 characters\n')
+
+    check2: bool = True
+    while check2:
+        playlist_description :str = input('Input a description for your new playlist [Max 300 char]: ')
+        if len(playlist_name) <= 300:
+            check2 = False
+        else:
+            print('Playlist description must be less than 300 characters')
+
+    check3: bool = True
+    while check3:
+        privacy: str = input('Playlist must be public or private?: ')
+        if privacy.lower() in ['public','private']:
+            check3 = False
+            privacy = privacy.lower()
+
+            if privacy == 'public':
+                privacy_status: bool = True
+            else:
+                privacy_status: bool = False
+
+        else:
+            print('Input a valid privacy status')
+    
+    try:
+        Spotify.playlist_create(conn, USER_ID, playlist_name,
+                                privacy_status, playlist_description)
+    except:
+        print('\nAn error has occurred')
+
+
+def show_playlists(conn: Spotify) -> None:
+    """
+    Show playlists [Max 50] on user id
+    """
+
+    clear()
+
+    playlists = Spotify.playlists(conn, USER_ID, 50, 0)
+
+    print(f'The user has {playlists.total} titled playlists on Spotify:\n')
+    for i in range(playlists.total):
+        print(f'{i + 1}. {playlists.items[i].name}')
